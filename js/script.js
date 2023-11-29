1307ae0101e3009a85098c59321a6d54d6700fa8
alegreco7 <alexgreco2002@gmail.com>
2023-11-29 14:18:21 +0100

    function generate grid

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById('generateGridButton').addEventListener('click', generateGrid);
+
+function generateGrid() {
+    const gridContainer = document.getElementById('gridContainer');
+    gridContainer.innerHTML = '';
+}
