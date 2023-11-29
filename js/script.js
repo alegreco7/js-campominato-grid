6924af7d11fca49f06b74b0d04c02fb9307de9dc
alegreco7 <alexgreco2002@gmail.com>
2023-11-29 15:06:38 +0100

    bonus

--- js/script.js
+++ js/script.js
@@ -5,12 +5,21 @@ function generateGrid() {
     gridContainer.innerHTML = '';
 
     for (let row = 0; row < 10; row++) {
-        
+
         for (let col = 0; col < 10; col++) {
           const cellNumber = row * 10 + col + 1;
           const cell = document.createElement('div');
           cell.className = 'cell';
           cell.textContent = cellNumber;
+
+          // BONUS
+          cell.addEventListener('click', function () {
+            // Colora la cella di azzurro
+            cell.style.backgroundColor = 'blue';
+            // Emetti un messaggio in console con il numero della cella cliccata
+            console.log('Cella cliccata:', cellNumber);
+          });
+
           gridContainer.appendChild(cell);
         }
     }
