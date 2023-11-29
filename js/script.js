84b437af8d7e4e77495f22b2a7706f817451b4ab
alegreco7 <alexgreco2002@gmail.com>
2023-11-29 14:23:38 +0100

    function generate grid complete

--- js/script.js
+++ js/script.js
@@ -3,4 +3,16 @@ document.getElementById('generateGridButton').addEventListener('click', generate
 function generateGrid() {
     const gridContainer = document.getElementById('gridContainer');
     gridContainer.innerHTML = '';
+
+    for (let row = 0; row < 10; row++) {
+        
+        for (let col = 0; col < 10; col++) {
+          const cellNumber = row * 10 + col + 1;
+          const cell = document.createElement('div');
+          cell.className = 'cell';
+          cell.textContent = cellNumber;
+          gridContainer.appendChild(cell);
+        }
+    }
 }
+
